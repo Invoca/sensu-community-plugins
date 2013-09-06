12248ed3fadf51a8033919ca50adb8ec09204bdf
Rahul Kapadia <rahul@ringrevenue.com>
2013-09-06 11:42:26 -0700

    61 sensu monitoring: modify check for unicorn exhaustion

--- plugins/unicorn/check_unicorn_exahaustion.rb
+++ plugins/unicorn/check_unicorn_exahaustion.rb
@@ -11,23 +11,25 @@ require 'net/http'
 class CheckUnicornExhaustion < Sensu::Plugin::Check::CLI
 
   option :warn,
-         :short => '-w ACTIVE',
-         :long => '--warn ACTIVE',
-         :proc => proc {|a| a.to_i },
-         :default => 10
+         :short => '-w ACTIVE, QUEUED',
+         :long => '--warn ACTIVE, QUEUED',
+         :proc => proc {|a| a.split(',').map {|t| (t == 'n' || t == 'N') ? 'n' : t.to_i } },
+         :default => [10,'n']
 
   option :crit,
-         :short => '-c QUEUED',
-         :long => '--crit QUEUED',
-         :proc => proc {|a| a.to_i },
-         :default => 0
+         :short => '-c ACTIVE, QUEUED',
+         :long => '--crit ACTIVE, QUEUED',
+         :proc => proc {|a| a.split(',').map {|t| (t == 'n' || t == 'N') ? 'n' : t.to_i } },
+         :default => ['n',0 ]
 
   def run
     response = Net::HTTP.get('127.0.0.1', '/_raindrops')
     active, queued = response.split("\n")[2..3].map { |line| line.split(":").last.to_i }
     message "Active #{active}, Queued #{queued}"
-    critical if queued > config[:crit]
-    warning if active > config[:warn]
+    (config[:crit][0] == 'n') || (critical if active > config[:crit][0])
+    (config[:crit][1] == 'n') || (critical if queued > config[:crit][1])
+    (config[:warn][0] == 'n') || (warning if active > config[:warn][0])
+    (config[:warn][1] == 'n') || (warning if queued > config[:warn][1])
     ok
   end
 end
